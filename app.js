5f73b2b2f673d4ec73ada64c6384321120bef4b8
Caio Sanches <102621072+CaioSanches7777777@users.noreply.github.com>
2024-02-23 08:54:54 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,5 +7,19 @@ export async function build(opts){
         return {hello: 'world'}
     });
 
+    const products = [
+        {id:1, name: 'Tomate', qtd: 20},
+        {id:2, name: 'Cebola', qtd: 50}
+    ]
+
+    app.get('/products', async (request, reply) => {
+        return products;
+    })
+    
+    app.get('/products/:id', async (request, reply) => {
+        app.logger.info('Produto requisitado' + request.params.id);
+        return{};
+    })
+
     return app;
 };
