8c68989635ffb5a713ce60c526bc21fbe7161323
Caio Sanches <102621072+CaioSanches7777777@users.noreply.github.com>
2024-02-23 08:53:40 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import {build} from './app.js';
 
-const options = {logger: true}
+//const options = { logger: true };
+//const app = await build({options});
 
-const app = await build({options});
+const app = await build({logger: true});
 
 await app.listen({port: 3000});
