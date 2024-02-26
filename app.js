364d3b1d12d459d2431b4620bf6c16aa5b846925
Caio Sanches <102621072+CaioSanches7777777@users.noreply.github.com>
2024-02-26 08:01:08 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,30 +3,44 @@ import fastify from 'fastify';
 export async function build(opts){
     const app = fastify(opts);
 
+    app.get('/error', (request, reply) => {
+        throw new Error('Erro no servidor');
+    });
+
     app.get('/', async (request, reply) => {
-        return {hello: 'world'};
+        return { hello: 'world' }
     });
 
     const products = [
-        {id:1, name: 'Tomate', qtd: 20},
-        {id:2, name: 'Cebola', qtd: 50}
+        {id: 1, name: 'Tomate', qtd: 20},
+        {id: 2, name: 'Cebola', qtd: 50}
     ]
 
     app.get('/products', async (request, reply) => {
         return products;
     });
-    
+
     app.post('/products', async (request, reply) => {
-        let product = request.body
-        return{product};
+        let product = request.body;
+        return {product};
     });
 
     app.get('/products/:id', async (request, reply) => {
-        app.logger.info('Produto requisitado' + request.params.id);
-        return{};
+        app.log.info('Produto requisitado> ' + request.params.id);
+        return {};
+    });
+    
+    app.delete('/products/:id', async (request, reply) => {
+        app.log.info('Produto para remover> ' + request.params.id);
+        return {};
     });
 
-    
+    app.setErrorHandler(async (error, request, reply) => {
+        request.log.error({ error });
+        reply.code(error.statusCode || 500);
+
+        return `Route ${request.url} causes an Internal Server Error.`;
+    });
 
     return app;
-};
+}
