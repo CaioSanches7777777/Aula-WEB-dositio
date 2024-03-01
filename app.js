d33d0a9b69aa4a0bc84e1afd74d6a3249020dcd0
Caio Sanches <102621072+CaioSanches7777777@users.noreply.github.com>
2024-03-01 08:08:08 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 import fastify from 'fastify';
 import createError from '@fastify/error';
 import fastifyStatic from '@fastify/static';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import spa from './routes/spa.js';
+import movies from './routes/movies.js';
+import products from './routes/products.js'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const MyCustomError = createError('MyCustomError', 'Something strange happened', 501); //mensagem de erro customisada
 
@@ -13,39 +12,14 @@ export async function build(opts){
     const app = fastify(opts);
 
     app.register(fastifyStatic, {
-        root: path.join(__dirname, 'public'),
+        root: `${import.meta.dirname}/public`,
         wildcard: false
     });
 
-    app.get('/*', async (request, reply) => {
-        request.log.info({params: request.params}, 'Hello from wildcard');
-        return raply.sendFile('index.html');
-    });
-
-    const products = [
-        {id: 1, name: 'Tomate', qtd: 20},
-        {id: 2, name: 'Cebola', qtd: 50},
-        {id: 3, name: 'Cenoura', qtd: 30}
-    ]
-
-    app.get('/products', async (request, reply) => {
-        return products;
-    });
-
-    app.post('/products', async (request, reply) => {
-        let product = request.body;
-        return {product};
-    });
-
-    app.get('/products/:id', async (request, reply) => {
-        app.log.info('Produto requisitado> ' + request.params.id);
-        return {};
-    });
+    app.register(spa);
+    app.register(movies);
+    app.register(products);
     
-    app.delete('/products/:id', async (request, reply) => {
-        app.log.info('Produto para remover> ' + request.params.id);
-        return {};
-    });
 
     app.get('/error', (request, reply) => {
         throw new MyCustomError();
